048147a3c05cb92633cd631a9ffcfb11f1971600
Samuel <samuel.duperry@coda-student.school>
2026-03-09 10:21:28 +0100

    modif dans le git .ignore

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module school.coda.samuel_hong_zhong.bataille_navale {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.almasb.fxgl.all;
+
+    opens school.coda.samuel_hong_zhong.bataille_navale to javafx.fxml;
+    exports school.coda.samuel_hong_zhong.bataille_navale;
+}
---
+++ src/main/java/school/coda/samuel_hong_zhong/bataille_navale/HelloController.java
@@ -0,0 +1,14 @@
+package school.coda.samuel_hong_zhong.bataille_navale;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
